8d7bc9d998754f4fb62c8ddd82205ab23a65fcef
prospb <olancheg@gmail.com>
2010-11-24 01:03:34 +0300

    arrrgh

--- app/helpers/poops_helper.rb
+++ app/helpers/poops_helper.rb
@@ -12,18 +12,21 @@ module PoopsHelper
   end
 
   def random_poop(category)
-    @@poops ||= Poop.by_category(category).approved.map(&:id)
-    watch_path @@poops[rand(@@poops.count)]
+    @@poops ||= Poop.by_category(category).approved
+    poops = @@poops.map(&:id) if @@poops
+    watch_path poops[rand(poops.count)]
   end
 
   def previous_poop(category, current)
-    @@poops ||= Poop.by_category(category).approved.map(&:id)
-    watch_path @@poops[@@poops.index(current)-1]
+    @@poops ||= Poop.by_category(category).approved
+    poops = @@poops.map(&:id) if @@poops
+    watch_path poops[poops.index(current)-1]
   end
 
   def next_poop(category, current)
-    @@poops ||= Poop.by_category(category).approved.map(&:id)
-    pos = @@poops.index(current)
-    watch_path @@poops[pos == @@poops.count-1 ? 0 : pos+1]
+    @@poops ||= Poop.by_category(category).approved
+    poops = @@poops.map(&:id) if @@poops
+    pos = poops.index(current)
+    watch_path poops[pos == poops.count-1 ? 0 : pos+1]
   end
 end
