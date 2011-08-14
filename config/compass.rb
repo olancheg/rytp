a37cd7a3d884c29df76f169212927cd0a30caf54
olancheg <olancheg@gmail.com>
2011-08-14 05:33:01 +0400

    added to compass config html5boilerplate

--- config/compass.rb
+++ config/compass.rb
@@ -1,3 +1,5 @@
+require 'html5-boilerplate'
+
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
