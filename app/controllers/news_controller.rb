c1608f9f7314cc6908c844d30e6eb6621b977362
olancheg <olancheg@gmail.com>
2011-08-24 17:14:21 +0400

    replaced instance variable with local for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,9 @@
 class NewsController < ApplicationController
   before_filter :require_authentication, :except => [:index, :show]
-  before_filter :find_new
 
   authorize_resource
 
   def index
-    @news = News.ordered.page params[:page]
-
     respond_to do |format|
       format.html
       format.js
@@ -23,36 +20,43 @@ class NewsController < ApplicationController
   end
 
   def create
-    @news = current_user.news.build(params[:news])
+    news = current_user.news.build(params[:news])
 
-    if @news.save
-      redirect_to @news, :notice => t(:'news.created')
+    if news.save
+      redirect_to news, :notice => t(:'news.created')
     else
       render :new
     end
   end
 
   def update
-    if @news.update_attributes(params[:news])
-      redirect_to @news, :notice => t(:'news.updated')
+    if news.update_attributes(params[:news])
+      redirect_to news, :notice => t(:'news.updated')
     else
       render :edit
     end
   end
 
   def destroy
-    @news.destroy
-    redirect_to_back_or news_index_path
+    news.destroy
+
+    redirect_to news_index_path
   end
 
   private
 
-  def find_new
+  def news
     @news ||= if params[:id]
       News.find(params[:id])
     else
       News.new(params[:news])
     end
   end
+  helper_method :news
+
+  def all_news
+    @all_news ||= News.ordered.page params[:page]
+  end
+  helper_method :all_news
 end
 
