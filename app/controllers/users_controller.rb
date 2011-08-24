b92b5f8ff26504a41d83b0e46a43ae3ff6cbedbe
olancheg <olancheg@gmail.com>
2011-08-24 17:21:51 +0400

    replaced instance variable with local for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   before_filter :require_authentication, :except => :show
   before_filter :set_feed_class, :only => :show
-  before_filter :find_user, :except => :show
 
   def update
-    if @user.update_attributes(params[:user])
-      redirect_to profile_path(current_user), :notice => t(:'user.updated')
+    if user.update_attributes(params[:user])
+      redirect_to profile_path(user), :notice => t(:'user.updated')
     else
       render :edit
     end
@@ -15,18 +14,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @poops = @user.filters_and_orders(params).page params[:page]
-
     respond_to do |format|
       format.html
-      format.js { render :layout => false, :locals => {:poops => @poops} }
+      format.js { render :layout => false, :locals => {:poops => poops} }
     end
   end
 
   private
 
-  def get_user
-    @user ||= current_user
+  def user
+    @user ||= if action_name == 'show'
+      User.find(params[:id])
+    else
+      current_user
+    end
   end
+  helper_method :user
+
+  def poops
+    @poops ||= user.filters_and_orders(params).page params[:page]
+  end
+  helper_method :poops
 end
