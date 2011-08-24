3dab7630ef44becadeb79818fdf5d66026f86d06
olancheg <olancheg@gmail.com>
2011-08-24 17:08:31 +0400

    replaced instance variable with local in contests

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,16 +1,9 @@
 class ContestsController < ApplicationController
   before_filter :require_authentication, :except => [:index, :show]
-  before_filter :find_contest
 
   authorize_resource
 
   def index
-    @contests = if can?(:manage, Contest)
-      Contest.scoped
-    else
-      Contest.active
-    end.page params[:page]
-
     respond_to do |format|
       format.html
       format.js { render :layout => false }
@@ -18,16 +11,9 @@ class ContestsController < ApplicationController
   end
 
   def show
-    @poops = Poop.unscoped do
-      @contest.poops.order('created_at DESC')
-    end
-
-    @poops = @poops.only_approved if cannot?(:reject, Poop)
-    @poops = @poops.page params[:page]
-
     respond_to do |format|
       format.html
-      format.js { render 'shared/update_page', :layout => false, :locals => { :poops => @poops, :id => params[:id] } }
+      format.js { render 'shared/update_page', :layout => false, :locals => { :poops => poops, :id => params[:id] } }
     end
   end
 
@@ -38,34 +24,56 @@ class ContestsController < ApplicationController
   end
 
   def create
-    if @contest.save
-      redirect_to @contest, :notice => t(:'contest.created')
+    if contest.save
+      redirect_to contest, :notice => t(:'contest.created')
     else
       render :new
     end
   end
 
   def update
-    if @contest.update_attributes(params[:contest])
-      redirect_to @contest, :notice => t(:'contest.updated')
+    if contest.update_attributes(params[:contest])
+      redirect_to contest, :notice => t(:'contest.updated')
     else
       render :edit
     end
   end
 
   def destroy
-    @contest.destroy
+    contest.destroy
 
     redirect_to contests_path, :notice => t(:'contest.deleted')
   end
 
   private
 
-  def find_contest
+  def contest
     @contest ||= if params[:id]
       Contest.find(params[:id])
     else
       Contest.new(params[:contest])
     end
   end
+  helper_method :contest
+
+  def contests
+    @contests ||= if can?(:manage, Contest)
+      Contest.scoped
+    else
+      Contest.active
+    end.page params[:page]
+  end
+  helper_method :contests
+
+  def poops
+    return @poops if @poops
+
+    @poops = Poop.unscoped do
+      contest.poops.order('created_at DESC')
+    end
+
+    @poops = @poops.only_approved if cannot?(:reject, Poop)
+    @poops = @poops.page params[:page]
+  end
+  helper_method :poops
 end
